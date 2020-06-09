06efa84238fac4d93be74128ffc22e829dc1ad50
Bene Arinci <benedetta.arinci@gmail.com>
2020-06-09 22:46:33 +0100

    created Class Thermostat. Has starting temperature of 20 degree. It has 2 methods for increasing and decreasing the temperature by 1

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+describe('Thermostat', function () {
+  var thermostat;
+  beforeEach(function () {
+    thermostat = new Thermostat();
+  });
+
+  describe('thermostat', function () {
+    it('starts at 20 degrees', function () {
+      expect(thermostat.getCurrentTemperature()).toEqual(20);
+    });
+  });
+
+  describe('up', function() {
+    it('raises the temperature by 1 degree', function(){
+      thermostat.up()
+      expect(thermostat.getCurrentTemperature()).toEqual(21);
+    });
+  });
+
+  describe('down', function() {
+    it('decreases the temperature by 1 degree', function(){
+      thermostat.down()
+      expect(thermostat.getCurrentTemperature()).toEqual(19);
+    });
+  });
+
+
+
+//   describe('increase degrees', function () {
+//     it('increases the temperature', function () {
+//       thermostat.increase(15)
+//       expect(thermostat.degrees).toEqual(35);
+//     });
+//   });
+//
+//   describe('reduces degrees', function () {
+//     it('reduces the temperature', function () {
+//       thermostat.decrease(5)
+//       expect(thermostat.degrees).toEqual(15);
+//     });
+//   });
+//
+//   describe('minimum degrees', function () {
+//     it('raise an error if decreasing temperature under the minimum', function () {
+//       expect(function () {
+//         thermostat.decrease(15)
+//       }).toThrowError('you reached the minimum temperature');
+//     });
+//   });
+//
+//   describe('set a maximum temperature of 25 if power saving on', function () {
+//     it('set a max temperature of 25 if power saving on', function () {
+//       thermostat._isSavingMode(true)
+//       expect(thermostat.max).toEqual(25)
+//     });
+//   });
+//
+//   describe('set max temperature to 32 if power saving off', function () {
+//     it('sets the max to 32 is power saving off', function () {
+//       thermostat._isSavingMode(false)
+//       expect(thermostat.max).toEqual(32)
+//     });
+//   });
+//
+//   describe('set a maximum temperature of 25 if power saving on', function () {
+//     it('has true as default for isSavingMode', function () {
+//       thermostat._isSavingMode()
+//       expect(thermostat.max).toEqual(25)
+//     });
+//   });
+//
+//   describe('reset the temperature to 20 degrees', function () {
+//     it('has a function to reset the degrees to 20', function() {
+//       thermostat.decrease(7)
+//       thermostat.resetDegrees()
+//       expect(thermostat.degrees).toEqual(20)
+//     });
+//   });
+});
