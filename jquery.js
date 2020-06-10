936896b86ab7f84d6037eb7c3bd98d6e977eed86
Bene Arinci <benedetta.arinci@gmail.com>
2020-06-10 16:22:11 +0100

    set thermostat API

--- jquery.js
+++ jquery.js
@@ -32,6 +32,19 @@ $(document).ready( function() {
         $('.powermode').text(powermodeStatus());
         })
 
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=7b6fa8a41854ee697a2ff3a86c16eda9&units=metric', function(data) {
+    $('.current_temperature').text(data.main.temp);});
+
+    $('.submit_location').click(function() {
+      var city = $('.location').val()
+      $.get(setLocation(city), function(data) {
+        $('.current_temperature').text(data.main.temp);
+      })
+    })
+    
+    function setLocation(location) {
+      return 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=7b6fa8a41854ee697a2ff3a86c16eda9&units=metric'
+    }
 
     function powermodeStatus() {
         if (thermostat.isPowerSavingModeOn()) {
@@ -44,7 +57,7 @@ $(document).ready( function() {
     function energyUsageColor() {
         if (thermostat.energyUsage() === 'low-usage') {
             $('.energyusage').css('background-color', 'green');
-        } 
+        }
         else if (thermostat.energyUsage() === 'medium-usage') {
             $('.energyusage').css('background-color', 'black');
         } else {
